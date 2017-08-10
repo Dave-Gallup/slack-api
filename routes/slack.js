a03b1ae9af754c3de7690ce7b5571a7e4acd7045
Dave <wm.david.gallup@gmail.com>
2017-08-10 09:23:14 -0700

    Partial

--- routes/slack.js
+++ routes/slack.js
@@ -31,34 +31,82 @@ router.get('/auth', (req, res) => {
 
 function setEvents(io) {
   router.post('/events', (req, res) => {
-    writeMessage(
-      req.body.event.user,
-      req.body.event.text,
-      req.body.event.ts,
-      req.body.event.channel,
-    )
-      .then((message) => {
-        const channelId = message[0].channel_map_id;
-        const messageId = message[0].id;
 
-        const newMessage = {};
-        newMessage[channelId] = {}; // Slack's channel ID as key
-        newMessage[channelId][messageId] = {}; // Our message ID as key
+    switch(req.body.event.type){
 
-        newMessage[channelId][messageId].avatarImage = '';
-        newMessage[channelId][messageId].userId = message[0].user_map_id;
-        newMessage[channelId][messageId].name = req.body.event.user;// To be changed after MVP
-        newMessage[channelId][messageId].text = message[0].message;
-        newMessage[channelId][messageId].timestamp = message[0].message_timestamp;
-        newMessage[channelId][messageId].channelId = message[0].channel_map_id;
+      case 'message':
+        // message edited
+        if(req.body.event.previous_message){
+          console.log('Message edited', req.body.event);
+          break;
+        }
+        // message deleted
+        if (req.body.event.subtype
+          && req.body.event.subtype === 'message_deleted'){
+          console.log('Message deleted', req.body.event);
+          break;
+        }
+        // message posted
+        console.log('Message posted', req.body.event);
+        break;
 
+      case 'reaction_added':
+        console.log('Reaction added');
+        break;
 
-        io.sockets.emit('messages', newMessage);
+      case 'reaction_removed':
+        console.log('Reaction removed');
+        break;
 
+      //
+      case 'team_join':
+        console.log('User joined the team');
+        break;
 
-        analyzeSentimentAndSaveScore(io, message[0].channel_map_id);
-      });
+      case 'user_change':
+        console.log('User info changed');
+        break;
+
+      case 'tokens_revoked':
+        console.log('Token(s) revoked');
+        break;
+    }
     res.sendStatus(200);
+    // handleNewMessageEvent(io)
+    // .then((req,res) => {
+    //   res.sendStatus(200);
+    // })
+    // .catch(err => err);
+  });
+}
+
+function handleNewMessageEvent(io, req){
+  return writeMessage(
+    req.body.event.user,
+    req.body.event.text,
+    req.body.event.ts,
+    req.body.event.channel,
+  )
+  .then((message) => {
+    const channelId = message[0].channel_map_id;
+    const messageId = message[0].id;
+
+    const newMessage = {};
+    newMessage[channelId] = {}; // Slack's channel ID as key
+    newMessage[channelId][messageId] = {}; // Our message ID as key
+
+    newMessage[channelId][messageId].avatarImage = '';
+    newMessage[channelId][messageId].userId = message[0].user_map_id;
+    newMessage[channelId][messageId].name = req.body.event.user;// To be changed after MVP
+    newMessage[channelId][messageId].text = message[0].message;
+    newMessage[channelId][messageId].timestamp = message[0].message_timestamp;
+    newMessage[channelId][messageId].channelId = message[0].channel_map_id;
+
+
+    io.sockets.emit('messages', newMessage);
+
+
+    analyzeSentimentAndSaveScore(io, message[0].channel_map_id);
   });
 }
 
